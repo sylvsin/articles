298faf6f6ec746a891f4703ab0180deba298e4db
sylvain lubwimi <lubwimi@gmail.com>
2021-08-26 11:17:02 +0200

    Fetched articles categories list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,46 @@
 import './App.css';
 
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 
-function App() {
+export interface IArticle {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  no_of_ratings: number;
+  avg_rating: number;
+}
+
+const App: React.FC = () => {
+
+  const [articleRecord, setArticleRecord] = useState<Record<string, IArticle[]>>({}); // <Category, Article[]>
+
+  useEffect(() => {
+    fetch(" http://127.0.0.1:8000/api/articles", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token c4700a6e4a06bf26c8563b9b517c4aba84902379'   
+      }
+    })
+    .then(resp => resp.json())
+    .then(resp => {
+      const data:IArticle[] = resp
+      setArticleRecord(data.reduce((rec, article)=>{  //reduce() is a function for object like map() is for array. 
+        const category = article.category
+        if(!(category in rec)){
+          rec[article.category] = []
+        }
+        rec[category] = [...rec[category], article]
+        return {...rec}
+      }, {} as Record<string, IArticle[]>))
+    })
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,9 +49,18 @@ function App() {
       <div className="articles">
         <div className="sidebar">
           <h2>Categories</h2>
-          <h2 className="title">Title</h2>
+
+          {Object.keys(articleRecord).map(cat=>(  //CategoryList
+              // <p key={cat} onClick={ () => articleCatClicked(cat) }>
+              <p key={cat}>
+                {cat} 
+              </p>
+          ))}
+
+          {/* <h2 className="title">Title</h2> */}
         </div>
         <div className="main">
+          <h2>Title</h2>
           <h2 className="details">Details</h2>
           <h2>Rate it</h2>
         </div>
